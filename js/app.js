3e7efe87ea83dadaa2508bf420ec40944b13bed3
Francisco Fuentes <git@ffuent.es>
2018-05-21 01:17:26 -0400

    initial commit

---
+++ js/app.js
@@ -0,0 +1,233 @@
+/* Country List */
+
+function getCountries(country_list, callback){
+	// $.getJSON(COUNTRY_LIST,'',callback);
+	displayCountries(country_list);
+}
+
+function renderCountryList(result){
+	return `
+	<option value="${result[0]}">${result[1]}</option>
+	`;
+}
+
+function displayCountries(data){
+	var newHtml = '<option>Select a country...</option>'
+	var results = data.map(function(item, index){
+		return renderCountryList(item);
+	});
+	$('.js-query-dropdown').html(results);
+}
+
+// YouTube local trends
+
+var YOUTUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/videos';
+
+function getDataFromApiYT(searchTerm, callback){
+	var query = {'key': YT,
+				 'maxResults': '10',
+                 'part': 'id,snippet',
+                 'chart': 'mostPopular',
+                 'regionCode': searchTerm,
+                 'type': 'video'};
+     $.getJSON(YOUTUBE_SEARCH_URL, query, callback);
+
+}
+
+function renderResultYT(result) {
+	
+  return `
+    <div id="video">
+      <h3>
+      <a class="js-result-name" href="https://www.youtube.com/watch?v=${result.id}" target="_blank">${result.snippet.title}</a> by <a class="js-video-channel" target="_blank" href="https://www.youtube.com/channel/${result.snippet.channelId}" target="_blank">${result.snippet.channelTitle}</a></h3>
+      <p>: <span class="js-thumbnail-video"><a href="https://www.youtube.com/watch?v=${result.id}" target="_blank"><img src="${result.snippet.thumbnails.default.url}" alt="${result.snippet.title}" /></a></span></p>
+      
+    </div>
+  `;
+}
+
+
+function displayQueryResultsYT(data){
+	console.log(data);
+	var results = data.items.map(function(item, index){
+		return renderResultYT(item);
+	});
+	console.log(results);
+	$('.js-youtube-videos').html(results);
+
+}
+
+// Excerpt from Wikipedia
+
+var WIKIPEDIA_URL = 'https://en.wikipedia.org/w/api.php?callback=?';
+
+function getDataFromApiWP(country, callback){
+	var query = {
+	"action": "query",
+	"format": "json",
+	"prop": "extracts",
+	"titles": country,
+	"formatversion": "2",
+	"exintro": "1",
+	"redirects": "1"
+	};
+	$.getJSON(WIKIPEDIA_URL, query, callback);
+}
+
+function renderResultWP(result) {
+	
+  return `
+    <div id="wikipedia">
+      
+      <h2>${result.title}</h2>
+
+      <span class="wikipedia-text">
+      ${result.extract}
+      </span>
+      
+    </div>
+  `;
+}
+
+function displayQueryResultsWP(data){
+	var results = data.query.pages.map(function(item, index){
+		return renderResultWP(item);
+	});
+	
+	$('.js-wikipedia').html(results);
+
+}
+
+// News API
+
+var NEWS_URL = 'https://newsapi.org/v2/everything';
+
+function getDataFromAPINews(country, callback){
+	var date = new Date();
+	date.setDate(date.getDate() - 7);
+	var domains = 'abc.net.au,aljazeera.com,apnews.com,us.cnn.com,economist.com,theguardian.com,nytimes.com,theglobeandmail.com,wsj.com,washingtonpost.com';
+	var query = { 'apiKey': NEWS_FEED,
+				'q': country + " news",
+				'language' : 'en',
+				'sortBy': 'relevancy',
+				'from' : date.toISOString(),
+				'pageSize': 10,
+				'domains' : domains
+	};
+	$.getJSON(NEWS_URL, query, callback);
+}
+
+function renderResultNews(result){
+	var rd = new Date(result.publishedAt);
+	var pubDate = rd.getDate() + "-" + (rd.getMonth()+1) + "-" + rd.getFullYear() + " "
+	+ rd.getHours() + ":" + rd.getMinutes();
+	return `
+		<article>
+			<h3><a href="${result.url}" target="_blank">${result.title}</a></h3>
+			<p><img src="${result.urlToImage}" alt="${result.title}" /></p>
+			<p><small>${result.source.name} published ${pubDate}</small></p>
+			<p>${result.description}</p>
+		</article>
+	`;
+}
+
+function displayQueryResultsNews(data){
+	var results = data.articles.map(function(item, index){
+		return renderResultNews(item);
+	}); 
+	$('.js-news-articles').html(results);
+}
+
+// Last FM 
+
+var LASTFM_URL = 'http://ws.audioscrobbler.com/2.0/';
+
+function getDataFromApiLastFM(country_code, callback){
+	var query = { 'api_key': LAST_FM,
+				  'method' : 'geo.gettopartists',
+				  'format': 'json',
+				  'country': country_code,
+				  'limit': 10
+	};
+	$.getJSON(LASTFM_URL,query,callback);
+}
+
+function renderResultLastFM(result){
+	console.log(result);
+	return `
+	<div id="artist">
+		<p><img src="${result.image[3]['#text']}" alt="${result.name}"</p>
+		<h3><a href="${result.url}">${result.name}</a></h3>
+	</div>
+	`;
+}
+
+function displayQueryResultsLastFM(data){
+	var results = data.topartists.artist.map(function(item, index){
+		return renderResultLastFM(item);
+	});
+	$('.js-lastfm-artists').html(results);
+}
+
+// Flickr
+
+var FLICKR_URL = 'https://api.flickr.com/services/rest/';
+
+function getDataFromApiFlickr(country, callback){
+	var query = { 'api_key' : FLICKR,
+				  'method' : 'flickr.photos.search',
+				  'tags' : country,
+				  'sort' : 'relevance',
+				  'format' : 'json',
+				   'per_page' : '4',
+				  'nojsoncallback' : '1'
+	};
+	$.getJSON(FLICKR_URL, query, callback);
+}
+
+function renderResultFlickr(result){
+	console.log(result);
+	return `<article class="flickr-picture">
+	<p>${result.title}</p>
+	<p><img src="https://farm${result.farm}.staticflickr.com/${result.server}/${result.id}_${result.secret}.jpg
+" alt="${result.title}" class="picture" /></p>
+</article>
+	`;
+}
+
+function displayQueryResultsFlickr(data){
+	console.log(data);
+	var results = data.photos.photo.map(function(item, index){
+		return renderResultFlickr(item);
+	});
+	$('.js-flickr-photos').html(results);
+}
+// Form handler
+
+function handleCountryForm(){
+	$('#myForm').submit(function(event){
+		event.preventDefault();
+		var queryTarget = $('.js-query-dropdown :selected');
+		
+		if (queryTarget !== '') {
+			var country = queryTarget.text();
+			var country_code = queryTarget.val();
+			getDataFromApiFlickr(country, displayQueryResultsFlickr);
+			getDataFromApiYT(country_code, displayQueryResultsYT);
+			getDataFromApiWP(country, displayQueryResultsWP);
+			getDataFromAPINews(country, displayQueryResultsNews);
+			getDataFromApiLastFM(country, displayQueryResultsLastFM);
+			$('.js-flickr h2').text("Pictures from " + country);
+			$('.js-news h2').text("News from " + country);
+			$('.js-youtube h2').text("Popular videos in " + country);
+			$('.js-lastfm h2').text("Popular musicians in " +  country);
+
+		}
+	})
+}
+
+
+$(function(){
+	getCountries(country_list,displayCountries);
+	handleCountryForm();
+})
---
+++ js/last.js
@@ -0,0 +1,4 @@
+const LAST_FM = '2dd5228a357e371535646d36d51a4128';
+const NEWS_FEED = '3cd19d21bb8d4ccc9f6ab3bcf601aeaf';
+const FLICKR = 'aa9fb1d7309bc216e4b47c01915632be';
+const YT = 'AIzaSyAuNZ58WuJ3Bc5SWT4W_5IKYlPX0-OKIcA';
